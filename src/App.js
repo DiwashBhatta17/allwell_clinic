fb62ac054b5573b67ed2e36925632b1b7ad743e9
DiwashBhatta17 <bhattaaawash@gmail.com>
2023-08-20 19:41:01 +0545

    Service Page and Doctor Detail User Page

--- src/App.js
+++ src/App.js
@@ -2,21 +2,6 @@
 
 import './App.css';
 import DoctorDetailUser from './Page/Users/DoctorDetailUser';
-import FirstComponent from './Page/Users/LandingPage/FirstComponent';
-import SecontComponent from './Page/Users/LandingPage/SecontComponent';
-import ThirdComp from './Page/Users/LandingPage/ThirdComp';
-import ServiceCom1 from './Page/Users/ServicesPage/ServiceCom1';
-import UserFooter from './Page/Users/UserFooter';
-
-
-import Axioes from './pages/homework/Axioes';
-import AxiosGet from './pages/homework/AxiosGet';
-
-import Class from './pages/homework/Class';
-import Formhand from './pages/homework/Formhand';
-import UseReducebutton from './pages/homework/UseReducebutton';
-import UseReducer from './pages/homework/UseReducer';
-import Rout from './pages/homework/datafetch/Rout';
 
 
 
